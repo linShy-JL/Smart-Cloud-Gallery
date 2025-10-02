2535feae96c83ffdb6b38745afb9f9da13eb5cdb
linsky <13884092548@163.com>
2025-10-02 18:01:35 +0800

    添加上传图片的功能

---
+++ src/main/java/com/jinlin/yupicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.jinlin.yupicturebackend.mapper;
+
+import com.jinlin.yupicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-09-27 12:15:42
+* @Entity com.jinlin.yupicturebackend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ src/main/java/com/jinlin/yupicturebackend/service/PictureService.java
@@ -0,0 +1,59 @@
+package com.jinlin.yupicturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jinlin.yupicturebackend.model.dto.picture.PictureQueryRequest;
+import com.jinlin.yupicturebackend.model.dto.picture.PictureUploadRequest;
+import com.jinlin.yupicturebackend.model.dto.user.UserQueryRequest;
+import com.jinlin.yupicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jinlin.yupicturebackend.model.entity.User;
+import com.jinlin.yupicturebackend.model.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+* @author Lenovo
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2025-09-27 12:15:42
+*/
+public interface PictureService extends IService<Picture> {
+    /**
+     * 上传图片
+     * @param multipartFile
+     * @param pictureUploadRequest
+     * @param loginUser   判断用户有没有权限修改
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser);
+    /**
+     * 获取查询条件
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取图片包装类（脱敏 单条）
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+    /**
+     * 获取图片包装类（分页）
+     *
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 校验图片
+     * @param picture
+     */
+    void validPicture(Picture picture);
+}
